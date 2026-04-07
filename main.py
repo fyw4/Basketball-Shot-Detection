adc6f4ec6e8c86fdbacbf4022c26734c6a62f4bb
fyw4 <wangqingchuan92@126.com>
2026-04-07 08:33:43 +0800

    增加step打印

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from shot_detector import Shot_Detector
 
 detector = Shot_Detector(source="./IMG_4674.mp4", output_path="goal_clips", step=2, display_object_info=True, model="./bball_model.pt", verbose=False, record=True, device="cuda")
 makes, attempts = detector.run()
+print(f"step is {detector.step}")
 print(f"Successful shots: {makes}/{attempts}")

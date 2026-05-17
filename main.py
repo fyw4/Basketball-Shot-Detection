45467464e04d85aee379b9dbd26bf5787f02cdd7
unknown <wangqingchuan92@126.com>
2026-05-17 18:53:27 +0800

    实现对文件夹内所有视频文件进行进球片段截取

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from shot_detector import Shot_Detector
 import time
+import os
 
-detector = Shot_Detector(source="./IMG_4674.mp4", output_path="goal_clips", step=2, display_object_info=True, model="./bball_model.pt", verbose=False, record=True, device="cuda")
-makes, attempts = detector.run()
-print(f"step is {detector.step}")
-print(f"Successful shots: {makes}/{attempts}")
+current_dir = "E:\新建文件夹"
+filelist = os.listdir(current_dir)
+
+for file in filelist:
+    if file.endswith(".mov"):
+        detector = Shot_Detector(source=os.path.join(current_dir, file), output_path="goal_clips", step=2, display_object_info=True, model="./bball_model.pt", verbose=False, record=True, device="cuda")
+        makes, attempts = detector.run()
+        print(f"step is {detector.step}")
+        print(f"Successful shots: {makes}/{attempts}")
 
 time.sleep(10)
